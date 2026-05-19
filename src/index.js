91d5fc4a36b5194801768b08ba05129baa64161e
Anatoliy Korotenko <tolik.korotenko21@gmail.com>
2026-05-19 20:20:03 +0300

    Reset pass

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+/* eslint-disable prettier/prettier */
 'use strict';
+
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { authrouter } from './routes/auth.route.js';
+import { errorMiddleware } from './middlewares/errorMiddleware.js';
+import { profilerouter } from './routes/profile.route.profile.js';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_HOST,
+    credentials: true,
+  }),
+);
+
+app.use(authrouter);
+app.use('/profile', profilerouter);
+app.use(errorMiddleware);
+
+app.listen(PORT, () => {
+  console.log('Server is runing');
+});
